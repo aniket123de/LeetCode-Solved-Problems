a309845a772f1c2c524c772f3c3253cadc0c9902
Aniket De <aniketde1234@gmail.com>
2026-09-10 14:56:37 +0530

    Time: 1 ms (50.28%), Space: 19.4 MB (55.7%) - LeetHub

---
+++ 1360-number-of-days-between-two-dates/1360-number-of-days-between-two-dates.py
@@ -0,0 +1,5 @@
+class Solution:
+    def daysBetweenDates(self, date1: str, date2: str) -> int:
+        d1 = date.fromisoformat(date1)
+        d2 = date.fromisoformat(date2)
+        return abs((d1 - d2).days)
